fab8e0e51ea3351844d133c2db83bd3aa54082c6
perelyaev <perelyaev@gmail.com>
2025-12-27 23:49:10 +0800

    add components/sign-in.tsx

--- components/sign-in.tsx
+++ components/sign-in.tsx
@@ -1,6 +1,19 @@
+'use client'
+
+import { authClient } from '@/lib/auth-client'
 import { Button, Dialog, DialogClose, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle, DialogTrigger, Input, Label } from './ui'
+import { useState } from 'react'
 
 export function SignIn() {
+  const [email, setEmail] = useState('')
+
+  async function handleSignIn() {
+    await authClient.emailOtp.sendVerificationOtp({
+      email: email,
+      type: "sign-in",
+    });
+  }
+
   return (
      <Dialog>
       <form>
@@ -17,14 +30,11 @@ export function SignIn() {
           <div className="grid gap-4">
             <div className="grid gap-3">
               <Label htmlFor="email">Электронная почта</Label>
-              <Input id="email" name="email" />
+              <Input id="email" name="email" value={email} onChange={(e) => setEmail(e.target.value)}/>
             </div>
           </div>
           <DialogFooter>
-            {/* <DialogClose asChild>
-              <Button variant="outline">Cancel</Button>
-            </DialogClose> */}
-            <Button type="submit">Продолжить</Button>
+            <Button type="submit" onClick={handleSignIn}>Продолжить</Button>
           </DialogFooter>
         </DialogContent>
       </form>
--- lib/auth.ts
+++ lib/auth.ts
@@ -1,18 +1,31 @@
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
+import { emailOTP } from "better-auth/plugins"
 import prisma from './prisma'
+import VerificationOTP from '@/emails/verificationOTP'
+import { sendEmail } from './send-email'
+import { render } from '@react-email/render'
 
 export const auth = betterAuth({
     database: prismaAdapter(prisma, {
         provider: "postgresql",
     }),
-    emailAndPassword: { 
-        enabled: true, 
-    }, 
-    socialProviders: { 
-        github: { 
-            clientId: process.env.GITHUB_CLIENT_ID as string, 
-            clientSecret: process.env.GITHUB_CLIENT_SECRET as string, 
-        }, 
-    }, 
+    plugins: [
+        emailOTP({ 
+            async sendVerificationOTP({ email, otp, type }) { 
+                if (type === "sign-in") {
+                    const html = await render(VerificationOTP(otp))
+                    await sendEmail({
+                        to: email,
+                        subject: '',
+                        html: html
+                    })
+                } else if (type === "email-verification") { 
+                    // Send the OTP for email verification
+                } else { 
+                    // Send the OTP for password reset
+                } 
+            }, 
+        }) 
+    ] 
 });
